1c2603a6bb26f82433578a0f1deb70919bf72517
Timofii <timoxafos@gmail.com>
2021-12-25 01:37:07 +0200

    some changes

--- client/src/components/AddMyComponent/PricingListItem/index.js
+++ client/src/components/AddMyComponent/PricingListItem/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 
 const PricingListItem = ({ item, pricing}) => {
-   
-
   return (
     <li>
       {item}
-      <p style={{display: 'none'}}>{pricing[item]}</p>
+      <p style={{ display: "none" }}>{pricing[item]}</p>
     </li>
   );
 };
--- client/src/components/AddMyComponent/PricingListItems/index.js
+++ client/src/components/AddMyComponent/PricingListItems/index.js
@@ -1,33 +1,55 @@
 import React from "react";
+import cx from "classnames";
+import styles from "./PricingListItems.module.scss";
 import PricingListItem from "../PricingListItem";
+
+const borderColor = {
+  bronze: "#e0b48d",
+  gold: "#e8b954",
+  platinum: "#555",
+  managed: "#28d2d0",
+};
 
 const PricingListItems = ({
   items: { id, pricing, title, price, save },
   items,
 }) => {
-  const {...rest} = items["main section"];
+  const { ...rest } = items["main section"];
 
   const renderResurses = (str) => {
-     const  resources = Object.keys(rest[str])
-     
-          return <div>{resources} <p>{rest['resources'][resources]}</p></div>
-  }
-  
-  const renderPricingItems= (el, id) => {
-   return <PricingListItem key={id} pricing={rest} item={el === "resources" ? renderResurses(el) : el} />
-  }
+    const resources = Object.keys(rest[str]);
+
+    return (
+      <div>
+        {resources} <p>{rest[str][resources]}</p>
+      </div>
+    );
+  };
+
+  const renderPricingItems = (el, id) => {
+    return (
+      <PricingListItem
+        key={id}
+        pricing={rest}
+        item={el === "resources" ? renderResurses(el) : el}
+      />
+    );
+  };
 
   return (
-    <article>
-      <div>
+    <article className={styles.card}>
+      <div
+        style={{ border: `10px solid ${borderColor[pricing]}` }}
+        className={styles.card_header}
+      >
         <h1>{pricing}</h1>
         <h3>{title}</h3>
         <span>US${price}</span>
         <span>Save ${save}</span>
       </div>
-      <ul>
-        {Object.keys(rest).map(renderPricingItems)}
-      </ul>
+      <p>{items.description1}</p>
+      <p>{items.description2}</p>
+      <ul>{Object.keys(rest).map(renderPricingItems)}</ul>
     </article>
   );
 };
