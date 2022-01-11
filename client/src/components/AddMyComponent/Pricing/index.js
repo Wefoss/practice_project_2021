bfb74a586c7911b9d3108b8c382ce180ca0d70af
Timofii <timoxafos@gmail.com>
2022-01-11 16:36:30 +0200

    remove :hover add layoutgray

--- client/src/components/AddMyComponent/Pricing/index.js
+++ client/src/components/AddMyComponent/Pricing/index.js
@@ -2,17 +2,19 @@ import React, { useState, useEffect } from "react";
 import styles from "./Pricing.module.scss";
 import PricingListItems from "../PricingListItems";
 import pirsingContent from "./pricingContent.json";
+import LayaoutLightBg from "../LayoutLightBg";
 
 const Pricing = () => {
-
   return (
-    <section className={styles.pricing}>
-      <div className={styles.pricing_container}>
-        {pirsingContent.map((el) => (
-          <PricingListItems key={el.id} items={el} />
-        ))}
-      </div>
-    </section>
+    <LayaoutLightBg>
+      <section className={styles.pricing}>
+        <div className={styles.pricing_container}>
+          {pirsingContent.map((el) => (
+            <PricingListItems key={el.id} items={el} />
+          ))}
+        </div>
+      </section>
+    </LayaoutLightBg>
   );
 };
 
