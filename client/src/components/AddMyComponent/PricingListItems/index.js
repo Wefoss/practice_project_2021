1d0681a57a4a43ce1f89da42fbd8458a6689a37b
Timofii <timoxafos@gmail.com>
2022-01-15 15:50:34 +0200

    right adaptive open and closed

--- client/src/components/AddMyComponent/PricingListItems/index.js
+++ client/src/components/AddMyComponent/PricingListItems/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import cx from "classnames";
 import styles from "./PricingListItems.module.scss";
 import PricingListItemSection from "../PricingListItemSection";
@@ -15,9 +15,10 @@ const PricingListItems = ({
   items,
 }) => {
   const { ...rest } = items["main section"];
+  const closeAdapthWindown = useRef();
 
   const [toggle, setToggleContent] = useState(false);
-  const hideAndOpenMobailContent = cx(styles.card_adapth, {
+  const hideAndOpenMobailContent = cx({
     [styles.hide]: !toggle,
     [styles.open]: toggle,
   });
@@ -25,11 +26,25 @@ const PricingListItems = ({
   const cardBorder = cx(styles.card, styles[pricing], hideAndOpenMobailContent);
 
   const togglePanl = (e) => {
-    setToggleContent(toggle === false ? true : false);
+    e.target.closest("ARTICLE").toggleAttribute("closedCurrent");
+    if (e.target.closest("ARTICLE").hasAttribute("closedCurrent")) {
+      setToggleContent(true);
+    } else setToggleContent(false);
+
+    e.target.closest("ARTICLE").parentNode.childNodes.forEach((el) => {
+      if (+el.getAttribute("id", id) !== id) {
+        el.removeAttribute("closedCurrent");
+        el.classList.remove(styles.open);
+        el.classList.add(styles.closed);
+      } else {
+        el.classList.remove(styles.closed);
+        el.classList.add(styles.open);
+      }
+    });
   };
 
   return (
-    <article className={cardBorder}>
+    <article ref={closeAdapthWindown} id={id} className={cardBorder}>
       <div
         style={{ border: `10px solid ${borderColor[pricing]}` }}
         className={styles.card_header}
@@ -47,8 +62,8 @@ const PricingListItems = ({
           <span>{pricing}</span>
           <span>US${price}</span>
         </div>
-        {!toggle && <i onClick={togglePanl} className="fa fa-plus"></i>}
-        {toggle && <i onClick={togglePanl} className="fa fa-minus"></i>}
+         <span className={styles.icon_increment}><i onClick={togglePanl} className="fa fa-plus"></i></span>
+         <span className={styles.icon_decrement}><i onClick={togglePanl} className="fa fa-minus"></i></span> 
       </div>
 
       <PricingListItemSection items={rest} link={link} pricing={pricing} />
